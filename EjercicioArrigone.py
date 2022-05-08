e1ae1120900e8a865b7bed6fac8e50f6c8421e5b
Federico Arrigone <102627523+fedearrigone@users.noreply.github.com>
2022-05-08 15:27:12 -0300

    Update EjercicioArrigone.py

--- EjercicioArrigone.py
+++ EjercicioArrigone.py
@@ -1,57 +1,53 @@
-
-from pickle import TRUE
-
-
-vehiculo_existente = 0
 cant_habitantes = 0
 hogar = 0
 acu_menores = 0
 max_edad = 0
 min_edad = -99
 cant_vehiculo = 0
+vehiculo_existente = 0
 acum_total = 0
 
-integrantes_familia = int(input("Ingrese la cantidad de ingreantes : "))
+integrantes_familia = int(input("Ingrese la cantidad de ingreantes (pulse 0 para finalizar): "))
 
 while integrantes_familia != 0 :
-    menores = int(input("Ingrese la cantidad de ingreantes menores : "))
-    edad_mayor =int(input("Ingrese la edad e la persona mas vieja : "))
-    edad_joven=int(input("Ingrese la edad e la persona mas joven : "))
-    ingreso_familia=float(input("Ingrese ingreso salarial familiar : "))
+    menores = int(input("Ingrese la cantidad de ingreantes menores de edad : "))
+    edad_mayor =int(input("Ingrese la edad de la persona de mayor edad : "))
+    edad_joven=int(input("Ingrese la edad se la persona de menor edad : "))
+    ingreso_familia=float(input("Ingrese el ingreso salarial familiar : "))
     acum_total = acum_total + ingreso_familia
     vehiculo = (input("Posee vehiculo (s/n): "))
     if vehiculo == "s":
-        vehiculo_existente = int(input("Cuantos vehiculos: "))
+        vehiculo_existente = int(input("Cuantos vehiculos posee: "))
         cant_vehiculo = (cant_vehiculo + vehiculo_existente)
-    #cantidad de habitantes censados
-    cant_habitantes = cant_habitantes + integrantes_familia
-    hogar = hogar + 1
-    acu_menores = acu_menores + menores
-    #persona mas vieja y mas joven
-    if edad_mayor > max_edad:
-        max_edad = edad_mayor
+    integrantes_familia = int(input("Ingrese la cantidad de ingreantes (pulse 0 para finalizar) : "))    
 
-    integrantes_familia = int(input("Ingrese la cantidad de ingreantes : "))
-
-#promedio
-promedio = (cant_habitantes * hogar /100)
-#porcentaje menores
-porcentaje_menores = (acu_menores * cant_habitantes / 100)
+# calculamos la cantidad de habitantes censados:
+cant_habitantes = cant_habitantes + integrantes_familia
+hogar = hogar + 1
+    
+acu_menores = acu_menores + menores
+   
 #persona mas vieja y mas joven
+if edad_mayor > max_edad:
+    max_edad = edad_mayor
+if edad_joven > min_edad:
+    min_edad = edad_joven
+        
+promedio = (cant_habitantes / hogar)
+       
+porcentaje_menores = (acu_menores * cant_habitantes / 100)
+        
 promedio_salario = (acum_total / hogar )
-#porcentaje de vehiculos
-porcentaje_vehiculos = (cant_vehiculo * 100/ hogar)
+       
+porcentaje_vehiculos = (cant_vehiculo * hogar /100)
 
-if integrantes_familia == 0:
-    print("porcentaje de familia que poseen vehiculo ",porcentaje_vehiculos)
-    print("salario",promedio_salario)
-    print("acum total", acum_total)
-    print("cant_vehiculo",cant_vehiculo)
-    print("max edad",max_edad)
-    print(porcentaje_menores)
-    print(promedio)
-    print(cant_habitantes)
-    exit("Gracias por usar Censito")
+print("Cantidad de habitantes censados: {}".format(cant_habitantes))
+print("El promedio de habitantes por hogar es de: {}".format(promedio))
+print("El porcentaje de menores es de: {}% ".format(porcentaje_menores))
+print("porcentaje de familia que poseen vehiculo ",porcentaje_vehiculos)
+print("La persona mas vieja tiene: {} años y la mas joven {} años.".format(max_edad, min_edad))
+print("El promedio del ingreso salarial de todos los hogares censados es de: {} ".format(promedio_salario))
+print("Vehiculos censados: {}".format(cant_vehiculo))
 
 
 
