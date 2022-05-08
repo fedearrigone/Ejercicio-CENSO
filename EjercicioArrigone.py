eb47a1541af44140930ba343ee590c12fb8949dc
Federico Arrigone <102627523+fedearrigone@users.noreply.github.com>
2022-05-08 16:36:04 -0300

    Update EjercicioArrigone.py

--- EjercicioArrigone.py
+++ EjercicioArrigone.py
@@ -2,12 +2,12 @@ cant_habitantes = 0
 hogar = 0
 acu_menores = 0
 max_edad = 0
-min_edad = -99
+min_edad = 100
 cant_vehiculo = 0
 vehiculo_existente = 0
 acum_total = 0
 
-integrantes_familia = int(input("Ingrese la cantidad de intregantes (pulse 0 para finalizar): "))
+integrantes_familia = int(input("Ingrese la cantidad de integrantes (pulse 0 para finalizar): "))
 
 while integrantes_familia != 0 :
     menores = int(input("Ingrese la cantidad de integrantes menores de edad : "))
@@ -28,11 +28,26 @@ while integrantes_familia != 0 :
 # persona mas vieja y mas joven
     if edad_mayor > max_edad:
         max_edad = edad_mayor
-    if edad_joven > min_edad:
+    if edad_joven < min_edad:
         min_edad = edad_joven
+
     integrantes_familia = int(input("Ingrese la cantidad de integrantes (pulse 0 para finalizar) : ")) 
 # promedio cantidad de habitantes       
 promedio = (cant_habitantes / hogar)
+# porcentaje de menores       
+porcentaje_menores = (acu_menores * cant_habitantes / 100)
+# promedio salarial       
+promedio_salario = (acum_total / hogar )
+# porcentaje de vehiculos      
+porcentaje_vehiculos = (cant_vehiculo * hogar /100)
+
+print("Cantidad de habitantes censados: {}".format(cant_habitantes))
+print("El promedio de habitantes por hogar es de: {}".format(promedio))
+print("El porcentaje de menores es de: {}% ".format(porcentaje_menores))
+print("porcentaje de familia que poseen vehiculo: {}% ".format(porcentaje_vehiculos))
+print("La persona mas vieja tiene: {} años y la mas joven {} años.".format(max_edad, min_edad))
+print("El promedio del ingreso salarial de todos los hogares censados es de: {} ".format(promedio_salario))
+print("Vehiculos censados: {}".format(cant_vehiculo))
 # porcentaje de menores       
 porcentaje_menores = (acu_menores * cant_habitantes / 100)
 # promedio salarial       
